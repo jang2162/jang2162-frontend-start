f1217b56666e188d72660c29bae1b0daedcf0f13
jbh <jang2162_@naver.com>
2019-07-08 23:03:41 +0900

    spdy 제거

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-import {readFileSync} from 'fs';
 import env from 'json-env';
 import next from 'next';
-import * as spdy from 'spdy';
 import conf from '../next.config';
 import {getRequestHandler} from './lib/Router';
 
@@ -23,15 +21,10 @@ const server = express()
         process.exit(1);
     }
 
-    const certPath = env.getString('cert.cert', 'cert/self.crt');
-    const keyPath = env.getString('cert.key', 'cert/self.key');
     const host = env.getString('host', '127.0.0.1');
     const port= env.getNumber('port', 8000);
 
-    spdy.createServer({
-        key: readFileSync(keyPath),
-        cert: readFileSync(certPath)
-    }, server).listen(port, host, () => {
+    server.listen(port, host, () => {
         console.log(`listening on https://${host==='0.0.0.0' ? '127.0.0.1' : host}:${port}`);
     });
 })();
