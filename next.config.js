638a8b8ea53ca38d30db8d4d461c3c47917422a9
jbh <jbh@kbrainc.com>
2023-02-02 18:39:52 +0900

    parseDate datetime-local 값 대응

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
+const stailwc = require("stailwc/install");
 
 /**
  * @type {import('next').NextConfig}
  **/
-
 const nextConfig = {
     experimental: {
         appDir: true,
+        swcPlugins: [stailwc()],
     },
     env: {
         NEXT_PUBLIC_INTROSPECTION: fs.readFileSync('./introspection.json', 'utf-8'),
