a6eeffdd11c8e9e41d9eed2d3f528e38c8158c18
jang2162 <jang2162_@naver.com>
2023-06-13 09:12:54 +0900

    nextjs 13.4

--- src/lib/apollo/gqlFunctions.ts
+++ src/lib/apollo/gqlFunctions.ts
@@ -11,21 +11,18 @@ import {GqlWrapper} from '@/lib/apollo/GqlWrapper';
 export async function gqlQuery<T = any, TVariables extends OperationVariables = OperationVariables>(gqlWrapper: GqlWrapper<T, TVariables>, options?: {variables: TVariables} & Omit<QueryOptions, 'query'|'variables'|'context'>){
     return getClient().query<T>({
         ...options,
-        variables: options?.variables ?? undefined,
         query: gqlWrapper.getDocumentNode()
     });
 }
 export async function gqlMutate<T = any, TVariables extends OperationVariables = OperationVariables>(gqlWrapper: GqlWrapper<T, TVariables>, options?: {variables: TVariables} & Omit<MutationOptions, 'mutation'|'variables'|'context'>){
     return getClient().mutate<T>({
         ...options,
-        variables: options?.variables ?? undefined,
         mutation: gqlWrapper.getDocumentNode()
     });
 }
 export async function gqlSubscribe<T = any, TVariables extends OperationVariables = OperationVariables>(gqlWrapper: GqlWrapper<T, TVariables>, options?: {variables: TVariables} & Omit<SubscriptionOptions, 'query'|'variables'|'context'>){
     return getClient().subscribe<T>({
         ...options,
-        variables: options?.variables ?? undefined,
         query: gqlWrapper.getDocumentNode()
     });
 }
